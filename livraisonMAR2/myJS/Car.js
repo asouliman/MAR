9823be719a009ee4a76b02df80b0b629324b5529
Thomas Daniellou <thomas.danll@gmail.com>
2015-01-16 15:06:55 +0100

    Refactoring: Put car and Keyboard into class

---
+++ livraisonMAR2/myJS/Car.js
@@ -0,0 +1,53 @@
+if(typeof(ModulesLoader)=="undefined")
+{
+	throw "ModulesLoaderV2.js is required to load script FlyingVehicle.js" ;
+}
+// Loads dependencies and initializes this module
+ModulesLoader.requireModules(['threejs/three.min.js', 'Physics.js', 'DebugHelper.js']) ;
+
+var Car = function (x, y, z, theta) {
+	// Attributes
+	this.position = new THREE.Object3D();
+	this.floorSlope = new THREE.Object3D();
+	this.rotationZ = new THREE.Object3D();
+	this.mesh = new THREE.Object3D();
+
+	this.position.name = 'position';
+	this.floorSlope.name = 'floorSlope';
+	this.rotationZ.name = 'rotationZ';
+
+	// Bind attributes to build the car
+	this.position.add(this.floorSlope);
+	this.floorSlope.add(this.rotationZ);
+
+	this.floorSlope.matrixAutoUpdate = false;
+
+	this.position.position.x = x;
+	this.position.position.y = y;
+	this.position.position.z = z;
+	this.rotationZ.rotation.z = theta;
+};
+
+Car.prototype.getPosition = function() {
+	return this.position;
+};
+
+Car.prototype.setPosition = function (x, y, z) {
+	this.position.position.set(x, y, z);
+};
+
+Car.prototype.getRotationZ = function() {
+	return this.rotationZ;
+};
+
+Car.prototype.setRotationZ = function (angle) {
+	this.rotationZ.rotation.z = angle;
+};
+
+Car.prototype.setMatrix = function (matrix) {
+	this.floorSlope.matrix.copy(matrix);
+};
+
+Car.prototype.setMesh = function (mesh) {
+	this.mesh = mesh;
+};
---
+++ livraisonMAR2/myJS/Keyboard.js
@@ -0,0 +1,74 @@
+var Keyboard = function () {
+	document.addEventListener('keydown', Keyboard.onKeyDown, false);
+	document.addEventListener('keyup', Keyboard.onKeyUp, false);
+}
+
+Keyboard.keys = {
+	65: 'A',
+	66: 'B',
+	67: 'C',
+	68: 'D',
+	69: 'E',
+	70: 'F',
+	71: 'G',
+	72: 'H',
+	73: 'I',
+	74: 'J',
+	75: 'K',
+	76: 'L',
+	77: 'M',
+	78: 'N',
+	79: 'O',
+	80: 'P',
+	81: 'Q',
+	82: 'R',
+	83: 'S',
+	84: 'T',
+	85: 'U',
+	86: 'V',
+	87: 'W',
+	88: 'X',
+	89: 'Y',
+	90: 'Z'
+};
+
+// Key states
+Keyboard.state = {};
+
+/**
+ * Gets the key name from a keyCode
+ */
+Keyboard.keyName = function (keyCode) {
+	return Keyboard.keys[keyCode];
+}
+
+/**
+ *
+ */
+Keyboard.onKeyDown = function (event) {
+	// Get the keyName
+	var keyName = Keyboard.keyName(event.keyCode);
+	Keyboard.state[keyName] = true;
+}
+
+/**
+ *
+ */
+Keyboard.onKeyUp = function (event) {
+	// Get the keyName
+	var keyName = Keyboard.keyName(event.keyCode);
+	Keyboard.state[keyName] = false;
+}
+
+Keyboard.prototype.update = function() {
+
+};
+
+Keyboard.prototype.isDown = function (keyName) {
+	return Keyboard.state[keyName];
+};
+
+Keyboard.prototype.isUp = function (keyName) {
+	return Keyboard.state[keyName];
+};
+
